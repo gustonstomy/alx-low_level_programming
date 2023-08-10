8cb0fd1101f24f1de8541f98efd3f0fdcaa8e93e
gustonstomy <gustomstomy@gmail.com>
2023-08-10 11:42:56 -0700

    README commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main - Print to the standard error
  * Return: one 1 (success)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
-
