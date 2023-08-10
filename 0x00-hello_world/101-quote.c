4b81f6e329e95ccbd373ea3727b9c7a3531e4839
gustonstomy <gustomstomy@gmail.com>
2023-08-10 11:34:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Print to the standard error
+ * Return: one 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
+}
+
