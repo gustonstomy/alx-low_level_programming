ad1d3f0d378ca6fac34e3388fee2aec479663ef8
gustonstomy <gustomstomy@gmail.com>
2023-08-15 15:37:52 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_islower - Checks for lowerchase character
+*Return: 1 for lowercase else 0
+*/
+
+int _islower(int r)
+{
+
+if (r >= 97 && r <= 122)
+{
+return (1);
+}
+return (0);
+}
