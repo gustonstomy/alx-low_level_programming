b34ba50fe5776b230f5c63af1842f303cf8987d7
gustonstomy <gustomstomy@gmail.com>
2023-08-24 10:45:15 -0700

    advanced

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,9 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *rot13(char *);
 
 #endif
