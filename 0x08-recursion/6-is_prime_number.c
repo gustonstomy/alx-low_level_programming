46b1cb889ab8700f66ac171861d99761930c0198
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-08-31 00:06:57 +0000

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * primeNum - Makes possible to evaluate from 1 to n
+ * @i: same number as n
+ * @j: number that iterates from 1 to n
+ * Return: 0.
+ */
+int primeNum(int a, int b)
+{
+	if (i == j)
+		return (1);
+	else if (i % j == 0)
+		return (0);
+	return (prime2(i, j + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (primeNum(n, 2));
+}
