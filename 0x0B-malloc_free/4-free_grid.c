aaec4a7332934025ff9ec6f1dc89c2480d197dfb
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-09-05 17:59:26 +0000

    Update 4-free_grid.c
    mandatory

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,23 @@
-cdbdshxckjnm
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_grid - array
+* @grid: rows 
+* @height: columns 
+* Return: a pointer to a 2 dimensional or NULL
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	int *p;
+
+	for (i = 0; i < height; i++)
+	{
+		p = grid[i];
+		free(p);
+	}
+	free(grid);
+}
