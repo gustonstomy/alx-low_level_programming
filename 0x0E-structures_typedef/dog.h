4b1091e4c4943b51f99f6263176a2df774090e5f
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-09-11 20:39:41 +0000

    Update dog.h
    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-
-#endif
+/**
+ * struct dog - Dog
+ * @name: name
+ * @age: age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog - rename struct 
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif 
