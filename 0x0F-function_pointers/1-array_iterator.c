f0f7b6f85b8c89a7cae63abc22716d692eb9cef9
gustonstomy <gustomstomy@gmail.com>
2023-09-13 12:39:26 -0700

    correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,6 +4,7 @@
 * array_iterator - this functions iterates an array
 * @array: the array to iterate
 * @size: the size of the array.
+* @action: pointer array
 * Return: this is a void function no return
 */
 
