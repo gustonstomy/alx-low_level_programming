0a5ba35ed11613e63f6cfa191adf327b7b2ac076
gustonstomy <gustomstomy@gmail.com>
2023-09-14 15:31:54 -0700

    mandatory

---
+++ 0x10-variadic_functions/main.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check for ALX School students.
+ * Return: 0.
+ */
+int main(void)
+{
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
+}
