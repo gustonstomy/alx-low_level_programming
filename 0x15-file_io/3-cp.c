46f03d88e9458fc34155256a3b6eb0469a4349cc
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-10-03 14:38:19 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * main - copies the content of a file to another file
+ * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments
- * Return:  0 on success.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, a, b;
-	char buffer[BUFSIZ];
+	int fd_r, fd_w, r, a, b;
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	from = open(argv[1], O_RDONLY);
-	if (from < 0)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((r = read(from, buffer, BUFSIZ)) > 0)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (to < 0 || write(to, buffer, r) != r)
+		if (fd_w < 0 || write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(from);
+			close(fd_r);
 			exit(99);
 		}
 	}
@@ -37,14 +38,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	a = close(from);
-	b = close(to);
+	a = close(fd_r);
+	b = close(fd_w);
 	if (a < 0 || b < 0)
 	{
 		if (a < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (b < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to;
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
